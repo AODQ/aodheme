ab4e37eba06c36a40421ffe857373d0022b02875
aodq <aodqig@gmail.com>
2017-04-14 14:25:59 -0400

    fixed a few bugs relating to getting atom values

--- source/app.d
+++ source/app.d
@@ -20,45 +20,51 @@ mixin(grammar(`
 `));
 
 string Atom_Variant_Func_Gen ( string name ) {
-  import std.format;
+  import std.format, std.conv, std.string : toUpper;
   return q{
     auto R%s ( ) in {
-      assert(val.type == typeid(%s));
+      assert(val.type == typeid(%s), "Trying to use type " ~ val.type.to!string ~ " as %s");
     } body {
       return val.get!%s;
     }
-  }.format(name[0].toUpper ~ name[1..$], name, name);
+
+    this ( %s val_ ) {
+      val = val_;
+    }
+  }.format(name[0].toUpper.to!string ~ name[1..$], name, name, name, name);
 }
 
 struct Atom {
   import std.variant;
   private Variant val;
-public:
-  mixin(Atom_Variant_Func_Gen!"string");
-  mixin(Atom_Variant_Func_Gen!"int");
-  mixin(Atom_Variant_Func_Gen!"float");
+  mixin(Atom_Variant_Func_Gen("string") );
+  mixin(Atom_Variant_Func_Gen("float" ) );
+  mixin(Atom_Variant_Func_Gen("int"   ) );
 
   auto RList ( ) in {
     assert(val.type == typeid(Variant[]));
   } body {
     return val;
   }
+
+  auto RType ( ) { return val.type; }
 }
 
 Atom Func_Call ( Atom func, Atom[] args ) {
-  switch ( func.val ) {
+  auto fn_name = func.RString;
+  switch ( fn_name ) {
     default:
-      return Atom("UNKNOWN FUNC: " ~ func.val);
+      return Atom("UNKNOWN FUNC: " ~ fn_name);
     case "+":
-      return Atom(args[0].val ~ args[1].val);
+      if ( args[0].RType == typeid(int) )
+        return Atom(args[0].RInt + args[1].RInt);
+      return Atom(args[0].RFloat + args[1].RFloat);
   }
 }
 
 Atom Eval ( ParseTree expr ) {
-  import std.algorithm, std.array;
-  writeln("RECEIVED: ", expr);
+  import std.algorithm, std.array, std.conv : to;
   foreach ( ref atom; expr.children ) {
-    writeln("NOW: ", atom);
     switch ( atom.name ) {
       default:
         assert(false, "Unknown atom name: " ~ atom.name);
@@ -69,27 +75,16 @@ Atom Eval ( ParseTree expr ) {
       case "AOQ.Operator":
         return Atom(atom.matches[0]);
       case "AOQ.IntegerL":
-        return Atom(atom.matches[0]);
+        return Atom(atom.matches[0].to!int);
       case "AOQ.FloatL":
-        return Atom(atom.matches[0]);
+        auto m = atom.matches[0..$];
+        if ( m[$-1] == "f" ) m = m[0 .. $-1];
+        return Atom(m.joiner.array.to!float);
       case "AOQ.SExpr":
         auto func = atom.children[0].Eval;
         auto args = atom.children[1 .. $].map!(Eval).array;
-        writeln("FUNC: ", atom.children[0]);
-        writeln("FUNC: ", func);
+        writeln("ARGS: ", args);
         return Func_Call ( func, args );
-        // writeln("FUNC: ", atom.children[0]);
-        // writeln("ARGS: ", atom.children[1 .. $]);
-        // foreach ( ref sexpr; atom.children ) {
-        //   writeln("SEXPR: ", sexpr);
-        // }
-        // writeln("---");
-        // Atom func = atom.children[0].Eval;
-        // writeln("CHILD: ", atom.children[0].input);
-        // auto args = atom.children[1 .. $].map!(Eval).array;
-        // writeln("SIZE: ", atom.children.length);
-        // writeln("FUNC: ", func, " ARGS: ", args);
-      // break;
     }
   }
   return Atom("");
@@ -102,7 +97,16 @@ auto Evaluate ( string str_expression ) {
   return expression.Eval;
 }
 
+unittest {
+  // ints   ---
+  assert("(+ 1 2)".Evaluate.RInt() == 3);
+  // floats ---
+  assert("(+ 1.5f 2.0f)".Evaluate.RFloat() == 3.5f);
+  // sexprs ---
+  // assert("(+ (+ 1 2) (+ (+ 3 4) 5))".Evaluate.RInt() == 1+2+3+4+5);
+}
+
 void main() {
-  "(+ 1 2)".Evaluate.writeln;
+  "(+ 1.5f 2.0f)".Evaluate.writeln(" RESULT");
   // "(+ '(1 2.0f) somevar)".Evaluate.writeln;
 }
